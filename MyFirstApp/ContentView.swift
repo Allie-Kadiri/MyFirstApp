4e69be5ae87c4cdda2e3797453664eee942cb6af
Alesha Kadiri <aleshak2018@gmail.com>
2024-06-14 10:01:50 -0500

    Initial Commit

---
+++ MyFirstApp/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  MyFirstApp
+//
+//  Created by Scholar on 6/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+        }
+        .padding()
+        HStack {
+                Image("Satoru_Gojo")
+            }
+    }
+    }
+
+
+#Preview {
+    ContentView()
+}
